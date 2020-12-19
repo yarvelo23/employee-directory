303d8694e992d1109fb96a39e8259f94f3771ed0
yerson <yerson.a06@gmail.com>
2020-12-18 21:53:55 -0400

    navbar code

---
+++ employee-directory/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../styles/Navbar.css";
+
+function Navbar() {
+    return (
+      <div className="card-header bg-dark text-white">
+      Employee Directory
+      </div>
+
+    )
+
+}
+
+export default Navbar;
