66dd7b72dbf213243528534b22eb8c87baaa78a3
yerson <yerson.a06@gmail.com>
2020-12-18 21:51:22 -0400

    footer code

---
+++ employee-directory/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/Footer.css"
+
+function Footer() {
+    return (
+      <footer className="bg-dark footer text-white">
+        <div className="card-header bg-dark text-white">
+        &copy; FSC 2020
+        </div>
+
+      </footer>
+    );
+  }
+  
+  export default Footer;
